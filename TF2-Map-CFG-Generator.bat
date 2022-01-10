0c443f719e13c6e92bdbfc11019a1c53c9ca1971
Taureon <45183108+Taureon@users.noreply.github.com>
2022-01-10 19:34:50 +0100

    Create TF2-Map-CFG-Generator.bat

---
+++ TF2-Map-CFG-Generator.bat
@@ -0,0 +1,153 @@
+@echo off
+cd C:\Program Files (x86)\Steam\steamapps\common\Team Fortress 2\tf\cfg
+title TF2 Map CFG Generator
+color 04
+echo /------------------------WARNING----------------------\
+echo \ This adds a LOT of files to your tf/maps directory! /
+echo / Also it gets overwrites existing map.cfg files!     \
+echo \ (It only targets those 136 that come with the game) /
+echo / Close this terminal unless you are sure of this!    \
+echo \-----------------------------------------------------/
+pause
+color 0a
+echo Starting...
+echo //put your reset script here > mapreset.cfg
+echo exec mapreset.cfg > ctf_2fort.cfg
+echo exec mapreset.cfg > ctf_2fort_invasion.cfg
+echo exec mapreset.cfg > ctf_doublecross.cfg
+echo exec mapreset.cfg > ctf_doublecross_snowy.cfg
+echo exec mapreset.cfg > ctf_landfall.cfg
+echo exec mapreset.cfg > ctf_sawmill.cfg
+echo exec mapreset.cfg > ctf_snowfall_final.cfg
+echo exec mapreset.cfg > ctf_turbine.cfg
+echo exec mapreset.cfg > ctf_well.cfg
+echo exec mapreset.cfg > cp_5gorge.cfg
+echo exec mapreset.cfg > cp_badlands.cfg
+echo exec mapreset.cfg > cp_coldfront.cfg
+echo exec mapreset.cfg > cp_fastlane.cfg
+echo exec mapreset.cfg > cp_foundry.cfg
+echo exec mapreset.cfg > cp_freight_final1.cfg
+echo exec mapreset.cfg > cp_granary.cfg
+echo exec mapreset.cfg > cp_gullywash_final1.cfg
+echo exec mapreset.cfg > cp_metalworks.cfg
+echo exec mapreset.cfg > cp_powerhouse.cfg
+echo exec mapreset.cfg > cp_process_final.cfg
+echo exec mapreset.cfg > cp_sunshine_event.cfg
+echo exec mapreset.cfg > cp_snakewater_final1.cfg
+echo exec mapreset.cfg > cp_sunshine.cfg
+echo exec mapreset.cfg > cp_vanguard.cfg
+echo exec mapreset.cfg > cp_well.cfg
+echo exec mapreset.cfg > cp_yukon_final.cfg
+echo exec mapreset.cfg > cp_altitude.cfg
+echo exec mapreset.cfg > cp_dustbowl.cfg
+echo exec mapreset.cfg > cp_ambush_event.cfg
+echo exec mapreset.cfg > cp_egypt_final.cfg
+echo exec mapreset.cfg > cp_gorge.cfg
+echo exec mapreset.cfg > cp_gorge_event.cfg
+echo exec mapreset.cfg > cp_gravelpit.cfg
+echo exec mapreset.cfg > cp_junction_final.cfg
+echo exec mapreset.cfg > cp_manor_event.cfg
+echo exec mapreset.cfg > cp_mercenarypark.cfg
+echo exec mapreset.cfg > cp_mossrock.cfg
+echo exec mapreset.cfg > cp_mountainlab.cfg
+echo exec mapreset.cfg > cp_snowplow.cfg
+echo exec mapreset.cfg > cp_steel.cfg
+echo exec mapreset.cfg > cp_degrootkeep.cfg
+echo exec mapreset.cfg > cp_standin_final.cfg
+echo exec mapreset.cfg > tc_hydro.cfg
+echo exec mapreset.cfg > pl_badwater.cfg
+echo exec mapreset.cfg > pl_barnblitz.cfg
+echo exec mapreset.cfg > pl_bloodwater.cfg
+echo exec mapreset.cfg > pl_borneo.cfg
+echo exec mapreset.cfg > pl_breadspace.cfg
+echo exec mapreset.cfg > pl_fifthcurve_event.cfg
+echo exec mapreset.cfg > pl_cactuscanyon.cfg
+echo exec mapreset.cfg > pl_chilly.cfg
+echo exec mapreset.cfg > pl_enclosure_final.cfg
+echo exec mapreset.cfg > pl_frontier_final.cfg
+echo exec mapreset.cfg > pl_goldrush.cfg
+echo exec mapreset.cfg > pl_rumble_event.cfg
+echo exec mapreset.cfg > pl_hasslecastle.cfg
+echo exec mapreset.cfg > pl_millstone_event.cfg
+echo exec mapreset.cfg > pl_hoodoo_final.cfg
+echo exec mapreset.cfg > pl_pier.cfg
+echo exec mapreset.cfg > pl_coal_event.cfg
+echo exec mapreset.cfg > pl_precipice_event_final.cfg
+echo exec mapreset.cfg > pl_snowycoast.cfg
+echo exec mapreset.cfg > pl_swiftwater_final1.cfg
+echo exec mapreset.cfg > pl_terror_event.cfg
+echo exec mapreset.cfg > pl_thundermountain.cfg
+echo exec mapreset.cfg > pl_upward.cfg
+echo exec mapreset.cfg > pl_wutville_event.cfg
+echo exec mapreset.cfg > plr_bananabay.cfg
+echo exec mapreset.cfg > plr_hightower_event.cfg
+echo exec mapreset.cfg > plr_hightower.cfg
+echo exec mapreset.cfg > plr_nightfall_final.cfg
+echo exec mapreset.cfg > plr_pipeline.cfg
+echo exec mapreset.cfg > arena_badlands.cfg
+echo exec mapreset.cfg > arena_byre.cfg
+echo exec mapreset.cfg > arena_granary.cfg
+echo exec mapreset.cfg > arena_lumberyard_event.cfg
+echo exec mapreset.cfg > arena_lumberyard.cfg
+echo exec mapreset.cfg > arena_nucleus.cfg
+echo exec mapreset.cfg > arena_offblast_final.cfg
+echo exec mapreset.cfg > arena_ravine.cfg
+echo exec mapreset.cfg > arena_sawmill.cfg
+echo exec mapreset.cfg > arena_watchtower.cfg
+echo exec mapreset.cfg > arena_well.cfg
+echo exec mapreset.cfg > koth_badlands.cfg
+echo exec mapreset.cfg > koth_brazil.cfg
+echo exec mapreset.cfg > koth_cascade.cfg
+echo exec mapreset.cfg > koth_bagel_event.cfg
+echo exec mapreset.cfg > koth_viaduct_event.cfg
+echo exec mapreset.cfg > koth_lakeside_event.cfg
+echo exec mapreset.cfg > koth_harvest_final.cfg
+echo exec mapreset.cfg > koth_harvest_event.cfg
+echo exec mapreset.cfg > koth_highpass.cfg
+echo exec mapreset.cfg > koth_king.cfg
+echo exec mapreset.cfg > koth_lakeside_final.cfg
+echo exec mapreset.cfg > koth_slaughter_event.cfg
+echo exec mapreset.cfg > koth_lazarus.cfg
+echo exec mapreset.cfg > koth_los_muertos.cfg
+echo exec mapreset.cfg > koth_maple_ridge_event.cfg
+echo exec mapreset.cfg > koth_megalo.cfg
+echo exec mapreset.cfg > koth_undergrove_event.cfg
+echo exec mapreset.cfg > koth_moonshine_event.cfg
+echo exec mapreset.cfg > koth_nucleus.cfg
+echo exec mapreset.cfg > koth_probed.cfg
+echo exec mapreset.cfg > koth_sawmill.cfg
+echo exec mapreset.cfg > koth_synthetic_event.cfg
+echo exec mapreset.cfg > koth_slasher.cfg
+echo exec mapreset.cfg > koth_suijin.cfg
+echo exec mapreset.cfg > koth_viaduct.cfg
+echo exec mapreset.cfg > sd_doomsday_event.cfg
+echo exec mapreset.cfg > sd_doomsday.cfg
+echo exec mapreset.cfg > tr_dustbowl.cfg
+echo exec mapreset.cfg > tr_target.cfg
+echo exec mapreset.cfg > itemtest.cfg
+echo exec mapreset.cfg > cp_cloak.cfg
+echo exec mapreset.cfg > mvm_bigrock.cfg
+echo exec mapreset.cfg > mvm_coaltown.cfg
+echo exec mapreset.cfg > mvm_decoy.cfg
+echo exec mapreset.cfg > mvm_example.cfg
+echo exec mapreset.cfg > mvm_ghost_town.cfg
+echo exec mapreset.cfg > mvm_mannhattan.cfg
+echo exec mapreset.cfg > mvm_mannworks.cfg
+echo exec mapreset.cfg > mvm_rottenburg.cfg
+echo exec mapreset.cfg > rd_asteroid.cfg
+echo exec mapreset.cfg > ctf_foundry.cfg
+echo exec mapreset.cfg > ctf_gorge.cfg
+echo exec mapreset.cfg > ctf_hellfire.cfg
+echo exec mapreset.cfg > ctf_thundermountain.cfg
+echo exec mapreset.cfg > pass_brickyard.cfg
+echo exec mapreset.cfg > pass_district.cfg
+echo exec mapreset.cfg > pass_timbertown.cfg
+echo exec mapreset.cfg > pd_cursed_cove_event.cfg
+echo exec mapreset.cfg > pd_farmageddon.cfg
+echo exec mapreset.cfg > pd_monster_bash.cfg
+echo exec mapreset.cfg > pd_pit_of_death_event.cfg
+echo exec mapreset.cfg > pd_snowville_event.cfg
+echo exec mapreset.cfg > pd_watergate.cfg
+echo Finished! Added 136 map.cfg's!
+echo open up mapreset.cfg and start writing!
+pause
